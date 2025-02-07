51f1b5790ddd84e966371ebeb97200b81aa09479
เคเบิล <cbkcontactmail@gmail.com>
2025-02-07 15:22:25 +0700

    before add db context and delete relationship class

--- BaseLibrary/Entities/Employee.cs
+++ BaseLibrary/Entities/Employee.cs
@@ -2,6 +2,8 @@
 {
     public class Employee : BaseEntity
     {
+        public int Id { get; set; }
+        public String? Name { get; set; }
         public String? CivilId { get; set; }
         public String? FileNumber { get; set; }
         public String? Fullname { get; set; }
@@ -10,5 +12,15 @@
         public String? TelephoneNumber { get; set; }
         public String? Photo { get; set; }
         public String? Other { get; set; }
+
+        public GeneralDepartment? GeneralDepartment { get; set; }
+        public int GeneralDepartmentId { get; set; }
+        public Department? Department { get; set; }
+        public int DepartmentId { get; set; }
+        public Branch? Branch { get; set; }
+        public int BranchId { get; set; }
+        public Town? Town { get; set; }
+        public int TownId { get; set; }
+
     }
 }
