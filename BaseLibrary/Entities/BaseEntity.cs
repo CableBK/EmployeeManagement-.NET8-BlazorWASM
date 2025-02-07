f7b03cfd5de411ca2689dda2a6ab1f68cdd8c69b
เคเบิล <cbkcontactmail@gmail.com>
2025-02-07 15:00:39 +0700

    Add entities class

---
+++ BaseLibrary/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BaseLibrary.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public String? Name { get; set; }
+
+        [JsonIgnore]
+        public List<Employee>? Employees { get; set; }
+    }
+}
---
+++ BaseLibrary/Entities/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace BaseLibrary.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public String? CivilId { get; set; }
+        public String? FileNumber { get; set; }
+        public String? Fullname { get; set; }
+        public String? JobName { get; set; }
+        public String? Address { get; set; }
+        public String? TelephoneNumber { get; set; }
+        public String? Photo { get; set; }
+        public String? Other { get; set; }
+    }
+}
